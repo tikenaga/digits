886a68f5ac48f1f49b5494ceae4e90699549e2a5
tikenaga <ikenagat@hawaii.edu>
2020-11-02 13:37:38 -1000

    First run

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Icon, Header} from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -7,7 +7,6 @@ class Landing extends React.Component {
     return (
         <div className="landing-background">
           <Grid centered columns={3} stackable container>
-
             <Grid.Column textAlign='center'>
               <Icon size='large' name='user' inverted/>
               <Header as='h1' inverted>User Features</Header>
