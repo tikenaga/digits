ca5176583e71ea65166afc94df5d4865844e24b2
tikenaga <ikenagat@hawaii.edu>
2020-11-03 19:33:15 -1000

    Run 2

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -3,32 +3,11 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contacts/Contacts';
 import Contact from '../components/Contact';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListContacts extends React.Component {
-  contacts = [{
-    firstName: 'Philip', lastName: 'Johnson', address: 'POST 307, University of Hawaii',
-    image: 'https://philipmjohnson.github.io/images/philip2.jpeg',
-    description: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-        'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-  },
-    {
-      firstName: 'Henri', lastName: 'Casanova', address: 'POST 307, University of Hawaii',
-      image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
-      description: 'I am originally from France. I maintain a list of reports from my surf sessions. I have proof ' +
-          'that I ran the Hana relay with an actual Team.',
-    },
-    {
-      firstName: 'Kim', lastName: 'Binsted', address: 'POST 307, University of Hawaii',
-      image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-      description: 'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence' +
-          'from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of ' +
-          'punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as ' +
-          '"What do you call a Martian who drinks beer? An ale-ien!".',
-    },
-  ];
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -40,7 +19,7 @@ class ListContacts extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center">List Contacts</Header>
-          <Card.Group>{this.contacts.map((contacts, index) => <Contact key={index} contact={contacts}/>)}</Card.Group>
+          <Card.Group>{this.props.contacts.map((contacts, index) => <Contact key={index} contact={contacts}/>)}</Card.Group>
         </Container>
     );
   }
@@ -48,16 +27,16 @@ class ListContacts extends React.Component {
 
 /** Require an array of Stuff documents in the props. */
 ListContacts.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+  contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  const subscription = Meteor.subscribe(Contacts.userPublicationName);
   return {
-    stuffs: Stuffs.collection.find({}).fetch(),
+    contacts: Contacts.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
 })(ListContacts);
